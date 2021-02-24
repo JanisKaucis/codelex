575a7357166e12b1426207d09ccbb71bfaec8846
JanisKaucis <janiskaucis@gmail.com>
2021-02-24 19:42:54 +0000

    homework

--- basic-php/loops/ex5.php
+++ basic-php/loops/ex5.php
@@ -3,25 +3,26 @@
 class Piglet
 {
     public $userInput;
+    public $diceRoll;
 
-    public function __construct($userInput)
+    public function __construct($userInput,$diceRoll)
     {
         $this->userInput = $userInput;
+        $this->diceRoll = $diceRoll;
     }
 
 }
 
 $sum = 0;
-$piglet = new Piglet('y');
+$piglet = new Piglet('y',rand(1,6));
 echo 'Welcome to Piglet!' . PHP_EOL;
 
 while ($piglet->userInput == 'y') {
-    $diceRoll = rand(1, 6);
-    echo 'You rolled a ' . $diceRoll . '!' . PHP_EOL;
+    echo 'You rolled a ' . $piglet->diceRoll . '!' . PHP_EOL;
 
-    if ($diceRoll != 1) {
-        $sum += $diceRoll;
-        $piglet = new Piglet(readline('Roll again? Type : y for Yes and any other for No : '));
+    if ($piglet->diceRoll != 1) {
+        $sum += $piglet->diceRoll;
+        $piglet = new Piglet(readline('Roll again? Type : y for Yes and any other for No : '),rand(1,6));
 
     } else {
         die('You got 0 points.' . PHP_EOL);
