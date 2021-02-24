3e53fca1565a64b5ff7490e5652a932b463561e8
JanisKaucis <janiskaucis@gmail.com>
2021-02-24 20:03:06 +0000

    homework

--- basic-php/loops/ex2.php
+++ basic-php/loops/ex2.php
@@ -1,11 +1,11 @@
 <?php
 
-$i = readline('Input a number to multiply: ') . PHP_EOL;
-$n = readline('Input a number for how many times to multiply first number: ');
+$i = readline('Input a number 1: ') . PHP_EOL;
+$n = readline('Input a number 2: ');
 echo PHP_EOL;
 while ($n < 0) {
     echo 'You cannot do that!';
-    $n = readline('Input a number for how many times to multiply first number: ');
+    $n = readline('Input a number 2: ');
     echo PHP_EOL;
 }
 $result = 1;
