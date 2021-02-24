e4b5fd82421ed26a13062968203e6a0ce21be574
JanisKaucis <janiskaucis@gmail.com>
2021-02-24 19:36:39 +0000

    homework

--- basic-php/loops/ex3.php
+++ basic-php/loops/ex3.php
@@ -5,4 +5,9 @@ $secondWord = readline('Enter second word : ');
 $firstWordLength = strlen($firstWord);
 $secondWordLength = strlen($secondWord);
 
-echo $firstWord . str_repeat('.', 30 - $firstWordLength - $secondWordLength) . $secondWord;
+echo $firstWord;
+for ($i=0;$i<30-$firstWordLength-$secondWordLength;$i++){
+    echo '.';
+}
+echo $secondWord;
+//echo $firstWord . str_repeat('.', 30 - $firstWordLength - $secondWordLength) . $secondWord;
